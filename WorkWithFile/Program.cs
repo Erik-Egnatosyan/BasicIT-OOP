c905a07738441ca9566e1a3ce9acd02c1c7b2bd8
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-03-18 01:54:05 +0400

    Add work with file

--- WorkWithFile/Program.cs
+++ WorkWithFile/Program.cs
@@ -6,9 +6,41 @@ namespace WorkWithFile
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Users\Eriksson\Desktop\example.txt";
-            File.Create(filePath);
-            File.Exists(filePath);
+            string filePath = @"C:\Users\Erik\Desktop\example.txt";
+            if (File.Exists(filePath))
+            {
+                string content = File.ReadAllText(filePath);
+                Console.WriteLine("You have file");
+            }
+            else
+            {
+                File.Create(filePath).Dispose();
+                Console.WriteLine("You dont have it. I created for you");
+            }
+            string json = "{\"name\":\"John\", \"age\":30, \"car\":Bmw}";
+            File.WriteAllText(filePath, json + Environment.NewLine);
+            Console.WriteLine("Do you want to see 1)Pretty version or 2)Original?");
+            int inputOneOrTwo = Convert.ToInt32(Console.ReadLine());
+            if (inputOneOrTwo == 1)
+            {
+                string[] jsonReader = File.ReadAllText(filePath).Split(",");
+                for (int i = 0; i < jsonReader.Length; i++)
+                {
+                    jsonReader[i] = jsonReader[i].Replace("{", "").Replace("}", "");
+                    Console.WriteLine(jsonReader[i]);
+                }
+            }
+            else if (inputOneOrTwo == 2)
+                Console.WriteLine(File.ReadAllText(filePath));
+            Console.WriteLine("Do you want delite file?\n1)Yes\n2)No");
+            inputOneOrTwo = Convert.ToInt32(Console.ReadLine());
+            if (inputOneOrTwo == 1)
+            {
+                Console.WriteLine($"I delited your file");
+                File.Delete(filePath);
+            }
+            else if (inputOneOrTwo == 2)
+                Console.WriteLine("Okay i keep it ;3");
         }
     }
 }
