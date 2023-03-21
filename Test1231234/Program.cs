4710f40c10bcb78771988d3755d2cc9c1f54f98c
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-03-21 12:31:52 +0400

    Add generic and test

---
+++ Test1231234/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Test1231234
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "D1rt9";
+            str = string.Join(" ", str.ToCharArray());
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!(Char.IsLetter(str[i])) && str[i] != ' ')
+                {
+                    str = str.Remove(i - 1, 1);
+                }
+               else if (!(Char.IsLetter(str[i])) && Char.IsLetter(str[i + 1]) && i != str.Length)
+                    str = str.Remove(i, 1);
+            }
+            Console.WriteLine("This is str\n" + str);
+        }
+    }
+}
--- genericAdd/Program.cs
+++ genericAdd/Program.cs
@@ -4,11 +4,18 @@
     {
         static void Main(string[] args)
         {
-           Fooo<dynamic>(32,55);
+            Fooo<dynamic>(32,55);
+            Console.WriteLine(Sum(2, 3));
+            Console.WriteLine(Sum(2.4,2.5));
+            Console.WriteLine(Sum("Hello ", "World!"));
         }
         public static void Fooo<T>(T a, T b)
         {
             Console.WriteLine((dynamic)a + (dynamic)b);
+        }
+        public static T Sum<T>(T a, T b)
+        {
+            return (dynamic)a + (dynamic)b;
         }
     }
 }
