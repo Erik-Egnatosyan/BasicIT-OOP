cc2927df9ec677b37be45963ef0b5a61a41af3bb
Erik Egnatosyan <egnatosyan.sans@mail.ru>
2023-05-01 23:26:29 +0400

    array copy

--- ArrayCopy/Program.cs
+++ ArrayCopy/Program.cs
@@ -7,12 +7,13 @@
             int[] source = { 1, 2, 3 };
             int[] destination = new int[3];
 
-            Array.Copy(source, destination, source.Length);
-            source.CopyTo(destination, 0);
+            Array.Copy(source, destination, source.Length); //Метод Copy копирует элементы массива в новый массив с заданным размером. 
+            source.CopyTo(destination, 0); //Метод CopyTo копирует элементы массива в уже существующий массив, начиная с определенного индекса в этом массиве.
             foreach (int i in destination)
             {
                 Console.WriteLine(i);
             }
+            Convert.ToInt32(Co);
         }
     }
 }
